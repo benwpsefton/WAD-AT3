d4bd961ccb68a7053fa2e8f3c21da77ef62e2796
benwpsefton <benwpsefton@gmail.com>
2026-06-09 19:51:22 +0800

    changed back to standalone because it's stupid

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ if (isGithubActions && process.env.GITHUB_REPOSITORY) {
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: "export",
+  output: "standalone",
   images: {
     unoptimized: true,
   },
