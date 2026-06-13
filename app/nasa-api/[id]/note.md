76853299a0164e4c1693aa5e865bddbbbc4fe88d
benwpsefton <benwpsefton@gmail.com>
2026-06-14 02:33:36 +0800

    removed nasa API

--- app/nasa-api/[id]/note.md
+++
@@ -1,1 +0,0 @@
-# The dynamic route is removed so we can deploy the static site to github pages. - JohnR
--- app/nasa-api/layout.js
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <section className="py-6 md:py-10">
-      <main>{children}</main>
-    </section>
-  );
-}
